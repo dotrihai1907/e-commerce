dee713b3488bc21d457f47d2437116b89f3a97d0
Đỗ trí Hải <dotrihai1999@gmail.com>
2022-07-02 01:06:26 +0700

    Add effect on Sidebar at Screen Admin

--- src/pages/AdminScreen/Admin/index.jsx
+++ src/pages/AdminScreen/Admin/index.jsx
@@ -1,4 +1,5 @@
 import "antd/dist/antd.css";
+import clsx from "clsx";
 import styles from "./Admin.module.scss";
 
 import { MdDashboard } from "react-icons/md";
@@ -20,61 +21,154 @@ import { useState } from "react";
 import { Outlet } from "react-router-dom";
 
 function Admin() {
+  const [hiddenProduct, setHiddenProduct] = useState(true);
+  const [hiddenUser, setHiddenUser] = useState(true);
+  const [type, setType] = useState();
+  const [hide, setHide] = useState(false);
+  const [expand, setExpand] = useState(false);
+
+  const handleClickProduct = () => {
+    setHiddenProduct((prev) => !prev);
+  };
+
+  const handleClickUser = () => {
+    setHiddenUser((prev) => !prev);
+  };
+
+  const classNavigation = clsx(styles.navigation, {
+    [styles.hide]: hide,
+  });
+
+  const classContainer = clsx(styles.container, {
+    [styles.expand]: expand,
+  });
+
+  const handleClickToggleMenu = () => {
+    setHide((prev) => !prev);
+    setExpand((prev) => !prev);
+  };
+
   return (
     <div className={styles.dashboard}>
-      <div className={styles.navigation}>
+      <div className={classNavigation}>
         <div className={styles.logo}>
           <div className={styles.shopApp}>Shop App</div>
           <div className={styles.admin}>Admin</div>
         </div>
 
         <div className={styles.menu}>
-          <div className={styles.application}>Application</div>
-          <div className={styles.dashboard}>
-            <MdDashboard className={styles.iconDashboard} />
-            <p>Dashboard</p>
-          </div>
+          <p className={styles.application}>Application</p>
 
-          <div className={styles.product}>
-            <div className={styles.subMenuProduct}>
-              <FiDatabase className={styles.iconProduct} />
-              <p>Product</p>
-              <FiChevronDown className={styles.iconDownProduct} />
-            </div>
-            <div className={styles.itemMenuProduct}>
-              <div>Product List</div>
-              <div>Add Product</div>
-            </div>
-          </div>
+          <ul className={styles.list}>
+            <li
+              className={styles.item}
+              onClick={() => setType("Dashboard")}
+              style={type === "Dashboard" ? { backgroundColor: "#FFD333" } : {}}
+            >
+              <MdDashboard className={styles.icon} />
+              <p className={styles.text}>Dashboard</p>
+            </li>
 
-          <div className={styles.user}>
-            <div className={styles.subMenuUser}>
-              <FiUser className={styles.iconUser} />
-              <p>User</p>
-              <FiChevronDown className={styles.iconDownUser} />
-            </div>
-            <div className={styles.itemMenuUser}>
-              <div>User List</div>
-              <div>Add User</div>
-            </div>
-          </div>
+            <li>
+              <div className={styles.item} onClick={handleClickProduct}>
+                <FiDatabase className={styles.icon} />
+                <p className={styles.text}>Product</p>
+                {hiddenProduct ? (
+                  <FiChevronDown className={styles.chevron} />
+                ) : (
+                  <FiChevronUp className={styles.chevron} />
+                )}
+              </div>
+              {hiddenProduct && (
+                <ul>
+                  <li
+                    className={styles.subitem}
+                    onClick={() => setType("Product List")}
+                    style={
+                      type === "Product List"
+                        ? { backgroundColor: "#FFD333" }
+                        : {}
+                    }
+                  >
+                    Product List
+                  </li>
+                  <li
+                    className={styles.subitem}
+                    onClick={() => setType("Add Product")}
+                    style={
+                      type === "Add Product"
+                        ? { backgroundColor: "#FFD333" }
+                        : {}
+                    }
+                  >
+                    Add Product
+                  </li>
+                </ul>
+              )}
+            </li>
 
-          <div className={styles.orders}>
-            <FiShoppingCart className={styles.iconOrders} />
-            <p>Orders</p>
-          </div>
+            <li>
+              <div className={styles.item} onClick={handleClickUser}>
+                <FiUser className={styles.icon} />
+                <p className={styles.text}>User</p>
+                {hiddenUser ? (
+                  <FiChevronDown className={styles.chevron} />
+                ) : (
+                  <FiChevronUp className={styles.chevron} />
+                )}
+              </div>
+              {hiddenUser && (
+                <ul>
+                  <li
+                    className={styles.subitem}
+                    onClick={() => setType("User List")}
+                    style={
+                      type === "User List" ? { backgroundColor: "#FFD333" } : {}
+                    }
+                  >
+                    User List
+                  </li>
+                  <li
+                    className={styles.subitem}
+                    onClick={() => setType("Add User")}
+                    style={
+                      type === "Add User" ? { backgroundColor: "#FFD333" } : {}
+                    }
+                  >
+                    Add User
+                  </li>
+                </ul>
+              )}
+            </li>
 
-          <div className={styles.settings}>
-            <FiSettings className={styles.iconSettings} />
-            <p>Settings</p>
-            <FiChevronLeft className={styles.iconLeftSettings} />
-          </div>
+            <li
+              className={styles.item}
+              onClick={() => setType("Orders")}
+              style={type === "Orders" ? { backgroundColor: "#FFD333" } : {}}
+            >
+              <FiShoppingCart className={styles.icon} />
+              <p className={styles.text}>Orders</p>
+            </li>
+
+            <li
+              className={styles.item}
+              onClick={() => setType("Settings")}
+              style={type === "Settings" ? { backgroundColor: "#FFD333" } : {}}
+            >
+              <FiSettings className={styles.icon} />
+              <p className={styles.text}>Settings</p>
+              <FiChevronLeft className={styles.chevron} />
+            </li>
+          </ul>
         </div>
       </div>
 
-      <div className={styles.container}>
+      <div className={classContainer}>
         <div className={styles.header}>
-          <RiMenu2Fill className={styles.toggleMenu} />
+          <RiMenu2Fill
+            className={styles.toggleMenu}
+            onClick={handleClickToggleMenu}
+          />
 
           <Input
             prefix={<RiSearchLine />}
