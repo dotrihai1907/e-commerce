465f08fe00213052d60b27b4a30fa07392f04c35
Đỗ trí Hải <dotrihai1999@gmail.com>
2022-07-08 17:23:52 +0700

    edit

--- src/components/InfoProductDetail/InfoProductDetail.jsx
+++ src/components/InfoProductDetail/InfoProductDetail.jsx
@@ -1,5 +1,5 @@
 import { Radio } from "antd";
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { BsCartPlus } from "react-icons/bs";
 import style_css from "./InfoProductDetail.module.css";
 import style_less from "./InfoProductDetail.module.less";
